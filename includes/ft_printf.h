3af6546717eb1cf3b72924b54d0d9216fdf77f83
Quentin Robert de beauchamp <qurobert@z4r11p5.42lyon.fr>
2020-12-07 15:59:57 +0100

    start

---
+++ includes/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qurobert <qurobert@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/07 15:56:53 by qurobert          #+#    #+#             */
+/*   Updated: 2020/12/07 15:58:21 by qurobert         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+#endif
