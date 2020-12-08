82cffdcfc75a76950b2a92ab45a84f210cad1796
Quentin Robert de beauchamp <qurobert@z4r10p7.42lyon.fr>
2020-12-08 15:29:23 +0100

    save

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: qurobert <qurobert@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 21:27:25 by qurobert          #+#    #+#             */
-/*   Updated: 2020/12/08 13:53:59 by qurobert         ###   ########lyon.fr   */
+/*   Updated: 2020/12/08 15:22:03 by qurobert         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ typedef struct	s_flags
 	int 		minus;
 	int			width;
 	int			prec;
-	
-	
 }				t_flags;
 
 
--- srcs/parse.c
+++ srcs/parse.c
@@ -6,38 +6,71 @@
 /*   By: qurobert <qurobert@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 21:15:17 by qurobert          #+#    #+#             */
-/*   Updated: 2020/12/08 14:20:42 by qurobert         ###   ########lyon.fr   */
+/*   Updated: 2020/12/08 15:29:15 by qurobert         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 void	ft_parse_format(char *format, va_list ap, int *ret, int i)
 {
 	t_flags *arg;
+	
+	ft_parse_flags(format, ap, &i, arg);
+	ft_parse_width(format, ap, &i, arg);
+	ft_parse_prec(format, ap, &i, arg);
+}
 
-	while (format[i])
+void	ft_parse_flags(char *format, va_list ap, int *i, t_flags *arg)
+{
+	while (format[*i])
 	{
-		if (format[i] == '0')
+		if (format[*i] == '0')
 			arg->zero = 1;
-		if (format[i] == '-')
+		else
+			arg->zero = 0;
+		
+		if (format[*i] == '-')
 			arg->minus = 1;
-		if (format[i] == '*')
-			ft_parse_format(ft_itoa(va_arg(ap, int)), ap, ret, i);
-		while (format[i] >= '1' && format[i] <= '9')
-			arg->width = arg->width * 10 + format[i++] - '0';
-		i++;
 	}
 }
 
-int		ft_get_percent(char *s, int i)
+/*
+		if (format[i] == '*')
+		{
+			arg->width = va_arg(ap, int);
+			if (arg->width < 0)
+			{
+				arg->minus = 1;
+				arg->width = -arg->width;
+			}
+		}
+		while (format[i] >= '1' && format[i] <= '9')
+			arg->width = arg->width * 10 + format[i++] - '0';
+		if (format[i] == '.')
+		{
+			if (format[++i] == '*')
+				arg->prec = va_arg(ap, int);
+			while (format[i] >= '1' && format[i] <= '9')
+				arg->prec = arg->prec * 10 + format[i++] - '0';
+			if (arg->prec < 0)
+			{
+				arg->prec = 0;
+				i++;
+			}				
+		}
+		else
+			i++;
+	}
+}*/
+
+int		*ft_get_percent(char *format, int *index)
 {
-	while (s[i])
+	while (format[*index])
 	{
-		if (s[i] == '%')
-			return (i);
-		i++;
+		if (format[*index] == '%')
+			return (index);
+		(*index)++;
 	}
 	return (-1);
 }
@@ -47,7 +80,7 @@ void	ft_check_format(char *format, va_list ap, int *ret)
 	int index;
 	
 	index = 0;
-	if (ft_get_percent(format, index) != -1)
+	if (ft_get_percent(format, &index) != -1)
 		ft_parse_format(format, ap, ret, index);
 	else
 	{
